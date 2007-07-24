42b2dafabd8585fbaf6816750dbe5752f000f232
imdano <>
2007-07-24 05:05:15 +0000

    Added myself as a co-author :)

--- gui.py
+++ gui.py
@@ -894,7 +894,7 @@ class appGui:
         dialog = gtk.AboutDialog()
         dialog.set_name("Wicd")
         dialog.set_version(daemon.Hello())
-        dialog.set_authors([ "Adam Blackburn" ])
+        dialog.set_authors([ "Adam Blackburn", "Dan O'Reilly" ])
         dialog.set_website("http://wicd.sourceforge.net")
         dialog.run()
         dialog.destroy()

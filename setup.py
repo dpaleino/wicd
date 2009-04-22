167b18cdeb25b7415df06e028feaae828fba8673
Adam Blackburn <compwiz18@gmail.com>
2009-04-22 22:02:31 +0800

    Updated version number

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ import subprocess
 
 # Be sure to keep this updated!
 # VERSIONNUMBER
-VERSION_NUM = '1.6.0a1'
+VERSION_NUM = '1.6.0a2'
 # REVISION_NUM is automatically updated
 REVISION_NUM = 'unknown'
 CURSES_REVNO = 'uimod'

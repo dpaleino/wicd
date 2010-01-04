d9d6328ba10bbd50c7cccc6288bae8109d50a3cf
Adam Blackburn <compwiz18@gmail.com>
2010-01-03 22:47:32 -1000

    fix the case sensitivity in wicd-cli

--- cli/wicd-cli.py
+++ cli/wicd-cli.py
@@ -143,6 +143,7 @@ if options.network_details:
 # network properties
 
 if options.network_property:
+	options.network_property = option.network_property.lower()
 	if options.wireless:
 		is_valid_wireless_network_id(options.network)
 		if not options.set_to:

e59d76e9040996cd7e8756bf48531e69e3b486a0
Adam Blackburn <compwiz18@gmail.com>
2010-06-15 11:53:53 -0500

    updated setup.py, NEWS, and CHANGES for 1.7.1 release

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ import subprocess
 
 # Be sure to keep this updated!
 # VERSIONNUMBER
-VERSION_NUM = '1.7.0'
+VERSION_NUM = '1.7.1'
 # REVISION_NUM is automatically updated
 REVISION_NUM = 'unknown'
 CURSES_REVNO = 'uimod'

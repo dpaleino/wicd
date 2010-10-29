0e9eac0a890c5fd6d0e2929cffef9d5a00d5f379
Adam Blackburn <abkbrn@gmail.com>
2010-10-29 10:35:13 -0500

    update versions to 1.7.1b2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ import subprocess
 
 # Be sure to keep this updated!
 # VERSIONNUMBER
-VERSION_NUM = '1.7.1b1'
+VERSION_NUM = '1.7.1b2'
 # REVISION_NUM is automatically updated
 REVISION_NUM = 'unknown'
 CURSES_REVNO = 'uimod'

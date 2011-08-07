e10e1e9b48b9eb5c2273b3dc270be01988c52857
David Paleino <d.paleino@gmail.com>
2011-08-07 22:09:49 +0200

    generalize libc linking using ctypes' find_library()

--- wicd/misc.py
+++ wicd/misc.py
@@ -406,11 +406,8 @@ def RenameProcess(new_name):
         return False
     try:
         import ctypes
-        is_64 = os.path.exists('/lib64/libc.so.6')
-        if is_64:
-            libc = ctypes.CDLL('/lib64/libc.so.6')
-        else:
-            libc = ctypes.CDLL('/lib/libc.so.6')
+        from ctypes.util import find_library
+        libc = ctypes.CDLL(find_library('c'))
         libc.prctl(15, new_name, 0, 0, 0)
         return True
     except:

54ac41549a85f38c27c005b4ad9e399b31289a99
David Paleino <d.paleino@gmail.com>
2011-11-09 18:15:56 +0100

    Add myself to copyright header

--- wicd/configmanager.py
+++ wicd/configmanager.py
@@ -10,6 +10,7 @@ reusable for other purposes as well.
 #
 #   Copyright (C) 2008-2009 Adam Blackburn
 #   Copyright (C) 2008-2009 Dan O'Reilly
+#   Copyright (C) 2011      David Paleino
 #
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License Version 2 as
